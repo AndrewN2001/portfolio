6f22be2b9dd0dc79656a22b2114b52d49d67b6b6
ANDREW NGUYEN <andrew.nguyen.ta@gmail.com>
2023-12-23 02:01:08 -0600

    updated taskbar

--- src/components/taskbar.js
+++ src/components/taskbar.js
@@ -9,7 +9,7 @@ export default function Taskbar(){
             </div>
 
             <div className="fixed z-10 flex gap-5 text-[#EEE0C3] font-satoshi right-0 p-5 my-5 mx-16 rounded-md shadow-2xl bg-[#333335]/80 backdrop-blur-sm">
-                <Link to="/" className="">Home</Link>
+                <Link to="/portfolio_2">Home</Link>
                 <Link to='/experience'>Experience</Link>
                 <Link to="/contact">Contact</Link>
                 <a href="/December2023_Resume.pdf" target="_blank" download>Resume</a>
