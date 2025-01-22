157a98bef96af4ec5011814a95387e9f1dac12ae
Andrew Nguyen <andrew.nguyen.ta@gmail.com>
2025-01-21 22:03:39 -0600

    updated main page for mobile

--- src/App.js
+++ src/App.js
@@ -38,17 +38,17 @@ function App() {
         </div>
       </div>
 
-      <div id='secondary_page' className='min-w-screen bg-[#E7D3AB] py-20 px-16'>
-        <div id="about_me" className='grid grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-2'>
+      <div id='secondary_page' className='min-w-screen bg-[#E7D3AB] px-20 py-20'>
+        <div id="about_me" className='grid grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-2 pb-10'>
           <div className="flex flex-col justify-center">
             <h1 className='text-[#333335] font-satoshi font-bold italic text-4xl'>Who am I?</h1>
-            <h2 className='text-[#333335] font-satoshi text-2xl pt-6'>
+            <h2 className='text-[#333335] font-satoshi text-2xl pt-6 pr-6'>
               I am a senior at the University of Houston studying Computer Science, based in Houston, TX. Since 2020, I spend my days diving into the world of code and algorithms, constantly learning new technologies and expanding my knowledge and expertise. When I'm not writing code, you can find me at the gym creating new PRs, documenting my day-to-day adventures through my lens, or getting lost in the sounds of a new song genre. 
             </h2>
           </div>
 
-          <div id="image" className='mb-5 md:ml-5 flex justify-center items-center my-auto min-h-[25rem] min-w-[25rem] md:min-h-[40rem] md:min-w-[40rem] w-full'>
-              <img src={profilepic} className='h-[25rem] w-[25rem] md:h-[40rem] md:w-[40rem] rounded-full shadow-xl'/>
+          <div id="image" className='md:max-w-[40rem] md:max-h-[40rem] mx-auto'>
+              <img src={profilepic} className='max-w-full h-auto rounded-full shadow-xl' alt="Profile Picture"/>
           </div>
         </div>
 
@@ -57,16 +57,16 @@ function App() {
               Experience
           </h1>
 
-          <div className="grid grid-rows-1 grid-cols-3">
+          <div className="grid grid-rows-2 grid-cols-1 md:grid-rows-1 md:grid-cols-3 items-stretch">
             {experienceList.map((experience, index) => (
-              <div key={index} className='bg-[#333335] text-[#E7D3AB] text-center text-xl aspect-square'>
-                <h1 className='mt-5 ml-4 text-2xl italic'>
+              <div key={index} className='bg-[#333335] text-[#E7D3AB] text-center text-xl aspect-square flex flex-col py-5'>
+                <h1 className='ml-4 text-2xl italic'>
                   {experience.category}
                 </h1>
 
                 <div className='flex flex-wrap gap-2 px-3 items-center justify-center mt-5'>
-                  {experience.items.map((skill, index) => (
-                    <div key={index} className='shadow-2xl px-3 py-2 mt-1 bg-[#3d3d3f] hover:bg-[#525253] rounded-md'>
+                  {experience.items.map((skill, skillIndex) => (
+                    <div key={skillIndex} className='shadow-2xl px-3 py-2 mt-1 text-2xl md:text-xl bg-[#3d3d3f] hover:bg-[#525253] rounded-md'>
                       {skill}
                     </div>
                   ))}  
@@ -79,15 +79,15 @@ function App() {
         <div id="projects">
           <h1 className='text-[#333335] font-satoshi font-bold italic text-4xl pt-10'>Projects</h1>
           <div id="project_container" className='grid grid-cols-1 grid-rows-3 md:grid-cols-3 md:grid-rows-1 pt-6'>
-            <button className='bg-[#31539C] text-[#EEE0C3] text-left text-4xl italic hover:bg-[#223A6D] aspect-square flex flex-col gap-2 justify-center pl-8' onClick={() => setSelectProject("first")}>
+            <button className='bg-[#31539C] text-[#EEE0C3] text-left text-4xl md:text-3xl italic hover:bg-[#223A6D] aspect-square flex flex-col gap-2 justify-center pl-8' onClick={() => setSelectProject("first")}>
                 <h1>TopShape</h1>
                 <h1 className='text-base'>December 2024 - Ongoing</h1>
             </button>
-            <button className='bg-[#31539C] text-[#EEE0C3] text-left text-4xl italic hover:bg-[#223A6D] aspect-square flex flex-col gap-2 justify-center pl-8' onClick={() => setSelectProject("second")}>
+            <button className='bg-[#31539C] text-[#EEE0C3] text-left text-4xl md:text-3xl italic hover:bg-[#223A6D] aspect-square flex flex-col gap-2 justify-center pl-8' onClick={() => setSelectProject("second")}>
                 <h1>Voluntify</h1>
                 <h1 className='text-base'>August 2024 - December 2024</h1>
             </button>
-            <button className='bg-[#31539C] text-[#EEE0C3] text-left text-4xl italic hover:bg-[#223A6D] aspect-square flex flex-col gap-2 justify-center pl-8' onClick={() => setSelectProject("third")}>
+            <button className='bg-[#31539C] text-[#EEE0C3] text-left text-4xl md:text-3xl italic hover:bg-[#223A6D] aspect-square flex flex-col gap-2 justify-center px-8' onClick={() => setSelectProject("third")}>
                 <h1>Military Aircraft Detection Model</h1>
                 <h1 className='text-base text-left'>August 2024 - November 2024</h1>
             </button>
