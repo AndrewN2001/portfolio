f1bbf7e37e03692e6e16b002b3acc13651fecd88
Andrew Nguyen <andrew.nguyen.ta@gmail.com>
2025-10-30 10:23:19 -0500

    updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 export default function Footer(){
@@ -7,7 +6,7 @@ export default function Footer(){
         <div className="h-min w-full bg-[#333335] text-[#EEE0C3] gap-5 py-3 md:py-7 font-satoshi flex flex-col md:flex-row justify-between px-16 items-center text-lg md:text-xl">
             <h1 className="text-center">Andrew Nguyen 2023 | Design by <a href="https://johnnyle.io/" target="_blank" rel="noopener noreferrer">Johnny Le.</a></h1>
             <div className="md:visible invisible flex gap-5">
-                <Link to='/contact' className="hover:underline hover:underline-offset-4">Contact</Link>
+                <a href="mailto:andrew.nguyen.ta@gmail.com" className="hover:underline">Contact</a>
                 <a href="/Resume.pdf" className="hover:underline hover:underline-offset-4" target="_blank" download>Resume</a>
             </div>
             <div className="md:visible invisible flex gap-4 text-3xl">
